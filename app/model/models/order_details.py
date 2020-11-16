7c0fa9e016da1484cf2982d637a634cdb75c573e
Martin Karlsson <m.karlsson91@gmail.com>
2020-11-16 16:13:52 +0100

    Added classes to employee.py, order_details.py, store.py and store_inventory.py

---
+++ app/model/models/order_details.py
@@ -0,0 +1,20 @@
+from model.db import Base
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+
+
+class OrderDetails(Base):
+    __tablename__ = 'orderdetails'
+
+    ProductQuantity = sa.Column(sa.Integer, nullable=False)
+    PurchaseDate = sa.Column(sa.DATETIME, nullable=False)
+    idOrder = sa.Column(sa.Integer, sa.ForeignKey('order.idOrder'), primary_key=True, nullable=False)
+    idProduct = sa.Column(sa.Integer, sa.ForeignKey('product.idProduct'), primary_key=True, nullable=False)
+    idEmployee = sa.Column(sa.Integer, sa.ForeignKey('employee.idEmployee'), nullable=False)
+    Order = relationship("Order", back_populates="orderdetails")
+    Product = relationship("Product", back_populates="orderdetails")
+    Employee = relationship("Employee", back_populates="orderdetails")
+
+    def __repr__(self):
+        return f"ProductQuantity={self.ProductQuantity}, PurchaseDate={self.PurchaseDate}, idOrder={self.idOrder}, " \
+               f"idProduct={self.idProduct}, idEmployee={self.idEmployee}"
---
+++ app/model/models/store_inventory.py
@@ -0,0 +1,15 @@
+from model.db import Base
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+
+
+class StoreInventory(Base):
+    __tablename__ = 'storeinventory'
+
+    idProduct = sa.Column(sa.Integer, sa.ForeignKey('product.idProduct'), primary_key=True, nullable=False)
+    idStore = sa.Column(sa.Integer, sa.ForeignKey('store.idStore'), primary_key=True, nullable=False)
+    Product = relationship("Product", back_populates="storeinventory")
+    Store = relationship("Store", back_populates="storeinventory")
+
+    def __repr__(self):
+        return f"idProduct={self.idProduct}, idStore={self.idStore}"
