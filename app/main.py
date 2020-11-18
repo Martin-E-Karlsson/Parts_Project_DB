2300695505effbb405d7208f1c8e34e781f0e0f6
Martin Karlsson <m.karlsson91@gmail.com>
2020-11-18 09:32:18 +0100

    Added all imports from model.

--- app/main.py
+++ app/main.py
@@ -4,6 +4,14 @@ from model.models.product import Product
 from model.models.car import Car
 from model.models.source import Source
 from model.models.manufacturer import Manufacturer
+from model.models.company import Company
+from model.models.contact import Contact
+from model.models.employee import Employee
+from model.models.order import Order
+from model.models.order_details import OrderDetails
+from model.models.retailer import Retailer
+from model.models.store import Store
+from model.models.store_inventory import StoreInventory
 
 
 
