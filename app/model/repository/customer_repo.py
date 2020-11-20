4e94c5a3d3c4f99eca303e98515a2eef4ebebda8
Martin Karlsson <m.karlsson91@gmail.com>
2020-11-20 14:47:12 +0100

    Added accidentally removed files

--- app/model/repository/customer_repo.py
+++ app/model/repository/customer_repo.py
@@ -1,6 +1,31 @@
-def main():
-    pass
+from model.db import session
+from model.models.customer import Customer
 
 
-if __name__ == "__main__":
-    main()
+def insert_customer(address, id_contact):
+    new_customer = Customer(
+        Address=address,
+        idContact=id_contact
+    )
+    session.add(new_customer)
+    session.commit()
+
+
+def get_all_customers():
+    return session.query(Customer).all()
+
+
+def get_customer_by_id(id_customer):
+    return session.query(Customer).filter(Customer.idCustomer == id_customer).first()
+
+
+def get_customer_by_address(address):
+    return session.query(Customer).filter(Customer.Address.like(f"%{address}%").all())
+
+
+def change_customer_address(customer, new_address):
+    try:
+        customer.Address = new_address
+        session.commit()
+    except:
+        session.rollback()
