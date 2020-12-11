192fce5162a03751243c7bc8697a789efef64b98
Sayed Jawid Mortazawi <benjimortal@gmail.com>
2020-12-11 11:37:51 +0100

    fixed som line of code in retailer_repo

--- app/model/repository/retailer_repo.py
+++ app/model/repository/retailer_repo.py
@@ -1,37 +1,34 @@
-from model.db import session
-from model.models.retailer import Retailer
+from model.models.retailers import Retailer
 
 
 def insert_retailer(name, address, id_contact=None, id_manufacturer=None):
-    new_retailer = Retailer(
-        Name=name,
-        Address=address,
-        idContact=id_contact,
-        idManufacturer=id_manufacturer
-    )
-    session.add(new_retailer)
-    session.commit()
+    new_retailer = Retailer({"name": name, "address": address, "id_contact": id_contact, "id_manufacturer": id_manufacturer})
+    new_retailer.save()
+
 
 
 def get_all_retailers():
-    return session.query(Retailer).all()
+    return Retailer.all()
 
 
 def get_retailer_by_id(id_retailer):
-    return session.query(Retailer).filter(Retailer.idRetailer == id_retailer).first()
+    return Retailer.find(_id=id_retailer)
 
 
 def get_all_retailers_by_attribute(attribute_name, value):
     try:
-        return session.query(Retailer).filter(getattr(Retailer, attribute_name).like(f"%{value}%")).all()
+        return Retailer.find(**{attribute_name:value})
     except ValueError:
         print(f"The attribute_name; {attribute_name} was incorrect.")
 
 
-def change_retailer_attribute(retailer, attribute_name, new_value):
+def change_retailer_attribute(retailer_id, attribute_name, new_value):
     try:
-        setattr(retailer, attribute_name, new_value)
-        session.commit()
+        Retailer.change_attribute(retailer_id, attribute_name, new_value)
     except ValueError:
         print('Incorrect argument entered')
-        session.rollback()
+
+def insert_retailer_to_manufacturer(retailer_id, manufacturer_name, manufacturer_id):
+    Retailer.push_to_embedded_list(retailer_id, 'manufacturers',{'manufacturer_name': manufacturer_name, 'manufacturer_id':manufacturer_id})
+
+
